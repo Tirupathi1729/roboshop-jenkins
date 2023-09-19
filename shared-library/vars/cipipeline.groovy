f4756358b459e37244942e6199389a64c962b5bc
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-09-19 20:24:46 +0530

    jenkins

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,7 +1,16 @@
 def call() {
     node('workstation') {
         sh "find . | sed -e '1d' |xargs rm -rf"
-        git branch: "${BRANCH_NAME}", url: "https://github.com/Tirupathi1729/${component}"
+        //git branch: "${BRANCH_NAME}", url: "https://github.com/Tirupathi1729/${component}"
+        if(env.TAG_NAME ==~ "'*") {
+            env.branch_name ="refs/tags/${env.TAG_NAME}"
+        } else {
+            env.branch_name = "${env.BRANCH_NAME}"
+        }
+        checkout scmGit (
+                branches: [[name: branch_name]],
+                userRemoteConfigs: [[url:"https://github.com/Tirupathi1729/${component}"]]
+        )
         stage('Compile Code') {
             common.compile()
         }
