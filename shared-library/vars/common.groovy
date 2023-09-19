0c6c43a613507a98d3418d53981f6f070d81f7fd
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-09-19 16:43:26 +0530

    jenkins

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -1,9 +1,9 @@
 def compile() {
-    if (env.codeType =="nodejs") {
-        print 'NodeJs'
-    }
     if (env.codeType == "maven") {
         print 'Maven'
+    }
+    if (env.codeType =="nodejs") {
+        print 'NodeJs'
     }
     if (env.codeType == "python") {
         print 'python'
